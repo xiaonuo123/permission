2b11d69cf6c988270f1d9ead41f526205b7221f7
ernesto <124934522@qq.com>
2019-06-12 16:39:28 +0800

    add dialog hint logic

--- permission/src/main/java/com/ernesto/permission/PermissionUtil.java
+++ permission/src/main/java/com/ernesto/permission/PermissionUtil.java
@@ -1,24 +1,40 @@
 package com.ernesto.permission;
 
-import android.app.Activity;
+import android.app.Dialog;
 import android.content.Context;
-import android.content.pm.PackageInfo;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.content.pm.PackageManager;
-import android.content.pm.PermissionInfo;
+import android.os.Bundle;
+import android.provider.Settings;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.DialogFragment;
+import android.support.v4.app.FragmentActivity;
 import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AlertDialog;
+import android.text.TextUtils;
 
-import java.util.ArrayList;
-import java.util.List;
 
 public class PermissionUtil {
 
     private static PermissionUtil mInstance = new PermissionUtil();
 
-    private List<String> dangerousPermissions;
+    private static PermissionListener sListener;
 
-    private PermissionListener mListener;
+    private static String sDefPreliminaryExplanation;
+    private static String sDefPreliminaryPositive;
+    private static String sDefPreliminaryNegative;
+
+    private static String sDefFurtherExplanation;
+    private static String sDefFurtherPositive;
+    private static String sDefFurtherNegative;
+
+    private static boolean sIsDefaultStringInitialized;
+
+    private static String sFurtherExplanation;
+    private static String sFurtherPositive;
+    private static String sFurtherNegative;
 
     private PermissionUtil(){}
 
@@ -26,64 +42,44 @@ public class PermissionUtil {
         return mInstance;
     }
 
-    public void initialize(Context context) {
-        PackageInfo packageInfo = null;
-        try {
-            packageInfo = context.getPackageManager().getPackageInfo("android", PackageManager.GET_PERMISSIONS);
-        } catch (PackageManager.NameNotFoundException e) {
-            e.printStackTrace();
-        }
-        if (packageInfo != null && packageInfo.permissions != null) {
-            for (PermissionInfo permission : packageInfo.permissions) {
-                String protectionLevel;
-                switch(permission.protectionLevel) {
-                    case PermissionInfo.PROTECTION_NORMAL : protectionLevel = "normal"; break;
-                    case PermissionInfo.PROTECTION_DANGEROUS : protectionLevel = "dangerous"; break;
-                    case PermissionInfo.PROTECTION_SIGNATURE : protectionLevel = "signature"; break;
-                    case PermissionInfo.PROTECTION_SIGNATURE_OR_SYSTEM : protectionLevel = "signatureOrSystem"; break;
-                    default : protectionLevel = "<unknown>"; break;
-                }
-
-                if ("dangerous".equals(protectionLevel)) {
-                    if (dangerousPermissions == null)
-                        dangerousPermissions = new ArrayList<>();
-                    dangerousPermissions.add(permission.name);
-                }
-            }
-        }
-    }
-
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+    public void onRequestPermissionsResult(FragmentActivity context,int requestCode,
+                                           @NonNull String[] permissions, @NonNull int[] grantResults) {
         if (permissions.length > 0 && grantResults.length > 0) {
             if (grantResults[0] == -1) {
-                if (mListener != null) {
-                    mListener.onPermissionDenied();
-                    mListener = null;
-                }
-
+                showDialog(context,permissions[0],true,sFurtherExplanation,sFurtherPositive,sFurtherNegative);
             } else if (grantResults[0] == 0) {
-                if (mListener != null) {
-                    mListener.onPermissionGranted();
-                    mListener = null;
+                if (sListener != null) {
+                    sListener.onPermissionGranted();
+                    sListener = null;
                 }
             }
         }
     }
 
-    public void checkAndRequestPermission(Activity context,String permission,PermissionListener listener) {
+    public void checkAndRequestPermission(FragmentActivity context,String permission,PermissionListener listener){
+        this.checkAndRequestPermission(context,permission,listener,null,null,null,null,null,null);
+    }
+
+
+    public void checkAndRequestPermission(FragmentActivity context,String permission,PermissionListener listener,
+                                          String title,String positiveBtn,String negativeBtn,
+                                          String titleFurther,String positiveBtnFurther,String negativeBtnFurther) {
+        initializeDefaultString(context);
         int result = ContextCompat.checkSelfPermission(context,permission);
-        this.mListener = listener;
+        sListener = listener;
+
+        sFurtherExplanation = titleFurther;
+        sFurtherPositive = positiveBtnFurther;
+        sFurtherNegative = negativeBtnFurther;
 
         if (PackageManager.PERMISSION_GRANTED == result) {
-            if (mListener != null)
-                mListener.onPermissionGranted();
-            mListener = null;
+            if (sListener != null)
+                sListener.onPermissionGranted();
+            sListener = null;
         } else {
             if (ActivityCompat.shouldShowRequestPermissionRationale(context,
                     permission)) {
-                if (mListener != null)
-                    mListener.noNecessary();
-                mListener = null;
+                showDialog(context,permission,false,title,positiveBtn,negativeBtn);
             } else {
                 ActivityCompat.requestPermissions(context,
                         new String[]{permission},
@@ -92,9 +88,137 @@ public class PermissionUtil {
         }
     }
 
+    private void initializeDefaultString(Context context){
+        if (!sIsDefaultStringInitialized) {
+            sDefPreliminaryExplanation = context.getResources().getString(R.string.permission_preliminary_explanation);
+            sDefPreliminaryPositive = context.getResources().getString(R.string.permission_preliminary_positive);
+            sDefPreliminaryNegative = context.getResources().getString(R.string.permission_preliminary_negative);
+
+            sDefFurtherExplanation = context.getResources().getString(R.string.permission_further_explanation);
+            sDefFurtherPositive = context.getResources().getString(R.string.permission_further_positive);
+            sDefFurtherNegative = context.getResources().getString(R.string.permission_further_negative);
+            sIsDefaultStringInitialized = true;
+        }
+    }
+
     public interface PermissionListener {
         void onPermissionGranted();
         void onPermissionDenied();
-        void noNecessary();
     }
+
+
+    private void showDialog(FragmentActivity activity,String permission,boolean needJump2Setting,
+                            String title,String positiveBtn,String negativeBtn) {
+        DialogFragment newFragment = PermissionDialog.newInstance(permission,needJump2Setting,title,positiveBtn,negativeBtn);
+        newFragment.show(activity.getSupportFragmentManager(), "dialog");
+    }
+
+    public static class PermissionDialog extends DialogFragment {
+
+        public static PermissionDialog newInstance(String permission,boolean needJump2Setting,
+                                                   String title,String positiveBtn,String negativeBtn) {
+            PermissionDialog dialog = new PermissionDialog();
+            Bundle bundle = new Bundle();
+            bundle.putString("permission",permission);
+            bundle.putString("title", TextUtils.isEmpty(title) ?
+                    (needJump2Setting ? sDefFurtherExplanation : sDefPreliminaryExplanation)
+                    : title);
+            bundle.putString("positiveBtn",TextUtils.isEmpty(positiveBtn) ?
+                    (needJump2Setting ? sDefFurtherPositive : sDefPreliminaryPositive)
+                    : positiveBtn);
+            bundle.putString("negativeBtn",TextUtils.isEmpty(negativeBtn) ?
+                    (needJump2Setting ? sDefFurtherNegative : sDefPreliminaryNegative)
+                    : negativeBtn);
+            bundle.putBoolean("needJump2Setting",needJump2Setting);
+            dialog.setArguments(bundle);
+            return dialog;
+        }
+
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+        }
+
+        @NonNull
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+            if (getActivity() != null) {
+                final String permission = getArguments() == null ? "" : getArguments().getString("permission");
+                final String title = getArguments() == null ? "" : getArguments().getString("title");
+                final String positiveBtn = getArguments() == null ? "" : getArguments().getString("positiveBtn");
+                final String negativeBtn = getArguments() == null ? "" : getArguments().getString("negativeBtn");
+                final boolean needJump2Setting = getArguments() == null || getArguments().getBoolean("needJump2Setting");
+                return new AlertDialog.Builder(getActivity())
+                        .setTitle(title)
+                        .setCancelable(false)
+                        .setPositiveButton(positiveBtn,
+                                new DialogInterface.OnClickListener() {
+                                    public void onClick(DialogInterface dialog, int whichButton) {
+                                        if (needJump2Setting) {
+                                            startActivity(new Intent(Settings.ACTION_SETTINGS));
+                                        } else {
+                                            if (getActivity() != null)
+                                                ActivityCompat.requestPermissions(getActivity(),
+                                                        new String[]{permission},
+                                                        8879);
+                                        }
+                                    }
+                                }
+                        )
+                        .setNegativeButton(negativeBtn,
+                                new DialogInterface.OnClickListener() {
+                                    public void onClick(DialogInterface dialog, int whichButton) {
+                                        if (sListener != null)
+                                            sListener.onPermissionDenied();
+                                        sListener = null;
+                                    }
+                                }
+                        )
+                        .create();
+            } else {
+                return null;
+            }
+
+        }
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+    //    private List<String> dangerousPermissions;
+
+    //    public void initialize(Context context) {
+//        PackageInfo packageInfo = null;
+//        try {
+//            packageInfo = context.getPackageManager().getPackageInfo("android", PackageManager.GET_PERMISSIONS);
+//        } catch (PackageManager.NameNotFoundException e) {
+//            e.printStackTrace();
+//        }
+//        if (packageInfo != null && packageInfo.permissions != null) {
+//            for (PermissionInfo permission : packageInfo.permissions) {
+//                String protectionLevel;
+//                switch(permission.protectionLevel) {
+//                    case PermissionInfo.PROTECTION_NORMAL : protectionLevel = "normal"; break;
+//                    case PermissionInfo.PROTECTION_DANGEROUS : protectionLevel = "dangerous"; break;
+//                    case PermissionInfo.PROTECTION_SIGNATURE : protectionLevel = "signature"; break;
+//                    case PermissionInfo.PROTECTION_SIGNATURE_OR_SYSTEM : protectionLevel = "signatureOrSystem"; break;
+//                    default : protectionLevel = "<unknown>"; break;
+//                }
+//
+//                if ("dangerous".equals(protectionLevel)) {
+//                    if (dangerousPermissions == null)
+//                        dangerousPermissions = new ArrayList<>();
+//                    dangerousPermissions.add(permission.name);
+//                }
+//            }
+//        }
+//    }
 }
